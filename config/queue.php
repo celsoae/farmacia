8ef7aac2649f5907038252295e1978a00c6ee0eb
Celso FIlho <celsoae@gmail.com>
2024-04-16 10:59:38 -0400

    - Fix job queue

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sync'),
+    'default' => env('QUEUE_CONNECTION', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,8 +36,7 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => 'job_batches',
-//            'table' => 'jobs',
+            'table' => 'jobs',
             'queue' => 'default',
             'retry_after' => 90,
             'after_commit' => false,
